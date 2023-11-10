70d546e0c1a721c6312d480b14932b435405a774
Ander Cabrera <andercabrerac@gmail.com>
2023-11-10 06:57:28 -0600

    initial commit

---
+++ src/test/java/org/andercabrera/cotizador/CotizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andercabrera.cotizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0fc2d8c1d907018851f59708790ea2222ee47574
Ander Cabrera <andercabrerac@gmail.com>
2023-11-13 12:01:28 -0600

    modifying `database` indentation

--- database/CotizadorEnvios.sql
+++ database/CotizadorEnvios.sql
@@ -9,22 +9,22 @@ DROP TABLE IF EXISTS dbo.countries;
 GO
 
 CREATE TABLE dbo.users (
-	id_user   INT 		    NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	id_user   INT 	        NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	username  VARCHAR(120)  NOT NULL,
 	user_plan VARCHAR(120)  NOT NULL,
 	password  VARCHAR(35)   NOT NULL
 );
 
 CREATE TABLE dbo.regions (
-	id_region  INT			 NOT NULL  IDENTITY(1, 1) PRIMARY KEY,
+	id_region  INT		 NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	region     VARCHAR(120)  NOT NULL,
 );
 
 CREATE TABLE dbo.countries (
-	id_country  INT		      NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	country		VARCHAR(120)  NOT NULL,
-	id_region	INT           NOT NULL,
-	price		INT			  NOT NULL,
+	id_country  INT		  NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	country	    VARCHAR(120)  NOT NULL,
+	id_region   INT           NOT NULL,
+	price	    INT	  	  NOT NULL,
 
 	-- Foreign Key
 	CONSTRAINT FK_regions_countries
@@ -34,24 +34,24 @@ CREATE TABLE dbo.countries (
 
 INSERT INTO dbo.regions(region)
 	VALUES ('Europe'),
-		   ('Central America'),
-		   ('Asia');
+	       ('Central America'),
+	       ('Asia');
 
 INSERT INTO dbo.countries(country, id_region, price)
 	VALUES ('Spain', 1, 1000),
-		   ('Guatemala', 2, 700),
-		   ('Mexico', 2, 500),
-		   ('El Salvador', 2, 200),
-		   ('Honduras', 2, 300),
-		   ('Bélgica', 1, 2000),
-		   ('Croacia', 1, 1500),
-		   ('Mónaco', 1, 400),
-		   ('Polonia', 1, 700),
-		   ('Ucrania', 1, 1350),
-		   ('Israel', 3, 400),
-		   ('Qatar', 3, 700),
-		   ('Mongolia', 3, 1350),
-		   ('Japón', 3, 2000);
+	       ('Guatemala', 2, 700),
+	       ('Mexico', 2, 500),
+	       ('El Salvador', 2, 200),
+	       ('Honduras', 2, 300),
+	       ('Bï¿½lgica', 1, 2000),
+	       ('Croacia', 1, 1500),
+	       ('Mï¿½naco', 1, 400),
+	       ('Polonia', 1, 700),
+	       ('Ucrania', 1, 1350),
+	       ('Israel', 3, 400),
+	       ('Qatar', 3, 700),
+	       ('Mongolia', 3, 1350),
+	       ('Japï¿½n', 3, 2000);
 
 
 SELECT * FROM dbo.users;
